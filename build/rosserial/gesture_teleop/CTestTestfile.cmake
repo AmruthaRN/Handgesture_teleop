10f72641e22c9242376d27b256965a062aae1dcf
AmruthaRN <amrutha.anup@gmail.com>
2019-07-24 12:53:04 +0530

    Teleop

---
+++ build/rosserial/gesture_teleop/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/amrutha/blink_ws/src/rosserial/gesture_teleop
+# Build directory: /home/amrutha/blink_ws/build/rosserial/gesture_teleop
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
